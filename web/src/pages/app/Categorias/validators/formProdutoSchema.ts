cbfbef038b07484f88858139c5ac0ab8518cda03
Matheus-BezerraaVanguard <matheus.bezerra@sistemavangurd.com.br>
2024-11-10 17:17:37 -0300

    Começando fornecedores e categorias

---
+++ web/src/pages/app/Categorias/validators/formProdutoSchema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const formProdutoSchema = z.object({
+    nome: z.string().min(2, {
+        message: "Nome do produto deve ter no mínimo 2 caracteres",
+    }),
+    status: z.enum(["ativo", "inativo"]),
+    tipoControle: z.enum(["quantidade", "peso"]),
+    peso: z.number().or(z.string()).optional(),
+    quantidade: z.number().or(z.string()).optional(),
+    fornecedores: z.array(z.string()).optional(),
+    categorias: z.array(z.string()).optional()
+}).refine((data) => {
+    if (data.tipoControle === "peso") return data.peso && data.peso.toString().length > 0;
+
+    if (data.tipoControle === "quantidade") return data.quantidade && data.quantidade.toString().length > 0;
+    return true;
+}, {
+    message: "Campo Obrigatório conforme o tipo de controle escolhido",
+    path: ["tipoControle"]
+});
