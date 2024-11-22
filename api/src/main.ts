9fba2cbae9f1a2bd025069a3d4ba8ebd61acc23a
giovanni <br.giovanni@gmail.com>
2024-11-22 20:37:09 -0300

    feat: adicionando filter de erro conflict e not found

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,5 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaExceptionFilter } from './errors/filters/prisma-exception';
+import { HttpExceptionFilter } from './errors/filters/http-exception';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,6 +10,12 @@ async function bootstrap() {
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
+
+  app.useGlobalFilters(
+    new PrismaExceptionFilter(),  // Para erros do Prisma
+    new HttpExceptionFilter()     // Para erros HTTP gerais
+  );
+
   await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
